fd39802082a46a44bac8ca25967b9ca7bf81b6aa
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-08-14 15:25:20 -0500

    pan sword functionality (prob a bad idea)

--- src/main/java/net/mcreator/bioswrathweapons/item/BallsDelightfulPanItem.java
+++ src/main/java/net/mcreator/bioswrathweapons/item/BallsDelightfulPanItem.java
@@ -1,52 +1,130 @@
 
 package net.mcreator.bioswrathweapons.item;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.TooltipFlag;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import vectorwing.farmersdelight.common.item.SkilletItem;
 
 import java.util.List;
 
 public class BallsDelightfulPanItem extends SkilletItem {
+	private final Tier tier;
+	private final float attackDamage;
+	private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+
 	public BallsDelightfulPanItem(Block block) {
-		super(block, new Item.Properties().stacksTo(1)); //add sword stuff later
-//		super(new Tier() {
-//			public int getUses() {
-//				return 0;
-//			}
-//
-//			public float getSpeed() {
-//				return 9f;
-//			}
-//
-//			public float getAttackDamageBonus() {
-//				return 3f;
-//			}
-//
-//			public int getLevel() {
-//				return 4;
-//			}
-//
-//			public int getEnchantmentValue() {
-//				return 22;
-//			}
-//
-//			public Ingredient getRepairIngredient() {
-//				return Ingredient.of();
-//			}
-//		}, 3, -2.5f, new Item.Properties().fireResistant());
+		super(block, new Item.Properties().stacksTo(1).fireResistant().defaultDurability(0));
+		this.attackDamage = 6.0F;
+		ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+		builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", this.attackDamage, AttributeModifier.Operation.ADDITION));
+		builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", -2.5F, AttributeModifier.Operation.ADDITION));
+		this.defaultModifiers = builder.build();
+		this.tier = new Tier() {
+			public int getUses() {
+				return 0;
+			}
+
+			public float getSpeed() {
+				return 9f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 3f;
+			}
+
+			public int getLevel() {
+				return 4;
+			}
+
+			public int getEnchantmentValue() {
+				return 22;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of();
+			}
+		};
 	}
 
 	@Override
 	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, level, list, flag);
 		list.add(Component.literal("\u00A76The Grand Chef has arrived."));
+	}
+
+	public float getDamage() {
+		return this.attackDamage;
+	}
+
+	public boolean canAttackBlock(BlockState p_43291_, Level p_43292_, BlockPos p_43293_, Player p_43294_) {
+		return !p_43294_.isCreative();
+	}
+
+	public float getDestroySpeed(ItemStack p_43288_, BlockState p_43289_) {
+		if (p_43289_.is(Blocks.COBWEB)) {
+			return 15.0F;
+		} else {
+			return p_43289_.is(BlockTags.SWORD_EFFICIENT) ? 1.5F : 1.0F;
+		}
+	}
+
+	public boolean hurtEnemy(ItemStack p_43278_, LivingEntity p_43279_, LivingEntity p_43280_) {
+		p_43278_.hurtAndBreak(1, p_43280_, (p_43296_) -> {
+			p_43296_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+		});
+		return true;
+	}
+
+	public boolean mineBlock(ItemStack p_43282_, Level p_43283_, BlockState p_43284_, BlockPos p_43285_, LivingEntity p_43286_) {
+		if (p_43284_.getDestroySpeed(p_43283_, p_43285_) != 0.0F) {
+			p_43282_.hurtAndBreak(2, p_43286_, (p_43276_) -> {
+				p_43276_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+			});
+		}
+
+		return true;
+	}
+
+	public boolean isCorrectToolForDrops(BlockState p_43298_) {
+		return p_43298_.is(Blocks.COBWEB);
+	}
+
+	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot p_43274_) {
+		return p_43274_ == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(p_43274_);
+	}
+
+	@Override
+	public boolean canPerformAction(ItemStack stack, net.minecraftforge.common.ToolAction toolAction) {
+		return net.minecraftforge.common.ToolActions.DEFAULT_SWORD_ACTIONS.contains(toolAction);
+	}
+
+	public Tier getTier() {
+		return this.tier;
+	}
+
+	public int getEnchantmentValue() {
+		return this.tier.getEnchantmentValue();
+	}
+
+	public boolean isValidRepairItem(ItemStack p_43311_, ItemStack p_43312_) {
+		return this.tier.getRepairIngredient().test(p_43312_) || super.isValidRepairItem(p_43311_, p_43312_);
 	}
 }
