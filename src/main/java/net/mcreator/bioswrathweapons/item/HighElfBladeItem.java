691421cde24aaf464966facbd44bedac3d3371ed
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-08-14 13:12:05 -0500

    high elf slicer

--- src/main/java/net/mcreator/bioswrathweapons/item/HighElfBladeItem.java
+++ src/main/java/net/mcreator/bioswrathweapons/item/HighElfBladeItem.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.bioswrathweapons.item;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.TooltipFlag;
@@ -45,5 +48,14 @@ public class HighElfBladeItem extends SwordItem {
 	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, level, list, flag);
 		list.add(Component.literal("\u00A7bDancing among the leaves and quartz."));
+	}
+
+	@Override
+	public boolean hurtEnemy(ItemStack stack, LivingEntity entity, LivingEntity attacker) {
+		super.hurtEnemy(stack, entity, attacker);
+		entity.addEffect(new MobEffectInstance(MobEffects.POISON, 60, 0), attacker);
+		entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 60, 0), attacker);
+		entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 240, 0), attacker);
+		return true;
 	}
 }
