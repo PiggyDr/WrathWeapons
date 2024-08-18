107052f67857599152335deb92cf4be759070b86
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-08-18 14:22:02 -0500

    tried to get curios to work, failed

--- src/main/java/net/mcreator/bioswrathweapons/item/SculkSaberItem.java
+++ src/main/java/net/mcreator/bioswrathweapons/item/SculkSaberItem.java
@@ -1,29 +1,20 @@
 
 package net.mcreator.bioswrathweapons.item;
 
-import com.github.sculkhorde.core.ModMobEffects;
-import com.github.sculkhorde.core.ModPotions;
-import net.minecraft.core.particles.ParticleOptions;
+//import com.github.sculkhorde.core.ModMobEffects;
+
+import net.mcreator.bioswrathweapons.procedures.SculkSaberLivingEntityIsHitWithToolProcedure;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.AreaEffectCloud;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.network.chat.Component;
-
-import net.mcreator.bioswrathweapons.procedures.SculkSaberLivingEntityIsHitWithToolProcedure;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
@@ -75,10 +66,9 @@ public class SculkSaberItem extends SwordItem {
 		player.getCooldowns().addCooldown(itemStack.getItem(), 400);
 		if (!level.isClientSide()) {
 			AreaEffectCloud aec = new AreaEffectCloud(level, player.getX(), player.getY(), player.getZ());
-			aec.setDuration(50);
 			aec.setRadius(5);
 			aec.setOwner(player);
-			aec.setPotion(new Potion(new MobEffectInstance(ModMobEffects.PURITY.get(), 1200))); //aec effect application is weird
+			//aec.setPotion(new Potion(new MobEffectInstance(ModMobEffects.PURITY.get(), 1200))); //aec effect application is weird
 			aec.setParticle(ParticleTypes.TOTEM_OF_UNDYING);
 			((ServerLevel)level).addFreshEntity(aec);
 		}
