1f56ebb5d3f70e09fb5847afde440ff5132b1bb8
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-08-22 14:29:11 -0500

    start work on dwarven legacy

--- src/main/java/net/mcreator/bioswrathweapons/item/DwarvenLegacyItem.java
+++ src/main/java/net/mcreator/bioswrathweapons/item/DwarvenLegacyItem.java
@@ -1,6 +1,18 @@
 
 package net.mcreator.bioswrathweapons.item;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.mcreator.bioswrathweapons.BiosWrathWeaponsMod;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.TooltipFlag;
@@ -9,10 +21,16 @@ import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 
 import java.util.List;
+import java.util.UUID;
 
 public class DwarvenLegacyItem extends PickaxeItem {
+
+	private static final UUID KNOCKBACK_UUID = UUID.fromString("9971fbfe-ba1b-40ba-a715-7d33dc398d33");
+
 	public DwarvenLegacyItem() {
 		super(new Tier() {
 			public int getUses() {
@@ -45,5 +63,29 @@ public class DwarvenLegacyItem extends PickaxeItem {
 	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, level, list, flag);
 		list.add(Component.literal("\u00A7dThe next light of the Dwarven Race."));
+	}
+
+	@Override
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+		if (slot == EquipmentSlot.MAINHAND) {
+			HashMultimap<Attribute, AttributeModifier> attributes = HashMultimap.create(super.getAttributeModifiers(slot, stack));
+			attributes.put(Attributes.ATTACK_KNOCKBACK, new AttributeModifier(KNOCKBACK_UUID, BiosWrathWeaponsMod.MODID + ":knockback_bonus", 2, AttributeModifier.Operation.ADDITION));
+			return ImmutableMultimap.copyOf(attributes);
+		} else {
+			return super.getAttributeModifiers(slot, stack);
+		}
+	}
+
+	@Override
+	public boolean mineBlock(ItemStack itemStack, Level level, BlockState state, BlockPos pos, LivingEntity entity) {
+		if (super.mineBlock(itemStack, level, state, pos, entity)) {
+			if (entity instanceof ServerPlayer player) {
+
+			} else {
+
+			}
+			return true;
+		}
+		return false;
 	}
 }
