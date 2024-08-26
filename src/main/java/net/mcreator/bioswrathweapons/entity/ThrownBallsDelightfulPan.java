262318cbe7a22831c03129c53af2da876044fe88
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-08-25 23:29:08 -0500

    tried to get chain ricochets on bdp, failed

--- src/main/java/net/mcreator/bioswrathweapons/entity/ThrownBallsDelightfulPan.java
+++ src/main/java/net/mcreator/bioswrathweapons/entity/ThrownBallsDelightfulPan.java
@@ -5,6 +5,7 @@ import net.mcreator.bioswrathweapons.init.BiosWrathWeaponsModEntities;
 import net.mcreator.bioswrathweapons.item.BallsDelightfulPanItem;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.core.Vec3i;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -12,16 +13,15 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Arrow;
@@ -31,19 +31,21 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.enchantment.FireAspectEnchantment;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.*;
+import org.joml.Vector3f;
 import vectorwing.farmersdelight.common.registry.ModSounds;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class ThrownBallsDelightfulPan extends AbstractArrow {
 
     private ItemStack item;
     private boolean isReturning;
+    private List<Entity> hitEntities = new ArrayList<>();
+    private long lastHitTime = -1;
     private static final EntityDataAccessor<Boolean> ID_FOIL = SynchedEntityData.defineId(ThrownBallsDelightfulPan.class, EntityDataSerializers.BOOLEAN);
     private static final EntityDataAccessor<Float> ID_INITIAL_Y_ROT = SynchedEntityData.defineId(ThrownBallsDelightfulPan.class, EntityDataSerializers.FLOAT);
     private static final EntityDataAccessor<Integer> ID_BOUNCES = SynchedEntityData.defineId(ThrownBallsDelightfulPan.class, EntityDataSerializers.INT);
@@ -70,7 +72,7 @@ public class ThrownBallsDelightfulPan extends AbstractArrow {
 
     @Override
     protected ItemStack getPickupItem() {
-        return item;
+        return item.copy();
     }
 
     public boolean isFoil() {
@@ -106,12 +108,21 @@ public class ThrownBallsDelightfulPan extends AbstractArrow {
             owner.playSound(ModSounds.ITEM_SKILLET_ATTACK_STRONG.get(), 0.5F, pitch);
         }
 
-        if (true) {
+        if (this.getBounces() > 4) {
+            this.startReturn(result);
+        }
+
+        AABB searchBox = new AABB(this.position().add(-5, -5, -5), this.position().add(5, 5, 5));
+        List<Mob> potentialTargets = this.level().getEntitiesOfClass(Mob.class, searchBox);
+        potentialTargets.removeAll(this.hitEntities.stream().filter(e -> e instanceof Mob).toList());
+        if (potentialTargets.isEmpty()) {
             this.startReturn(result);
         } else {
-            //TODO finish
+            this.bounceToEntity(result, potentialTargets.stream().max((e1, e2) -> (int) (position().distanceTo(e1.position()) - position().distanceTo(e2.position()))).get());
         }
+
         this.incrementBounces();
+        this.lastHitTime = this.level().getGameTime();
     }
 
     @Override
@@ -128,7 +139,7 @@ public class ThrownBallsDelightfulPan extends AbstractArrow {
         }
         if (this.isOnFire())
             fire += 5;
-        damage *= 0.75F;
+        damage *= 0.85F;
 
         DamageSource damageSource = this.damageSources().arrow(this, getOwner() == null ? this : getOwner());
         if (entity.hurt(damageSource, damage) && entity.getType() != EntityType.ENDERMAN) {
@@ -150,12 +161,31 @@ public class ThrownBallsDelightfulPan extends AbstractArrow {
                 livingOwner.setLastHurtMob(entity);
             }
         }
+
+        this.hitEntities.add(entity);
     }
 
-    public void startReturn(HitResult result) {
-        BiosWrathWeaponsMod.LOGGER.info("startReturn: " + result.getClass());
+    @Override
+    protected void onHitBlock(BlockHitResult result) {
+    }
+
+    public void bounceToEntity(HitResult result, Entity target) {
+        Vec3 push = target.position().subtract(this.position()).normalize();
+        double scalar = 0.25;// / (Mth.sqrt((float) push.length()) * 4);
+        push = push.multiply(0.25, 0.25, 0.25);
+        if (result instanceof BlockHitResult blockHitResult) {
+            Vec3i normal = blockHitResult.getDirection().getOpposite().getNormal();
+            push.add(normal.getX() * 0.1, normal.getY() * 0.1, normal.getZ() * 0.1);
+        }
+        this.setDeltaMovement(push);
+//        startReturn(result);
+    }
+
+    public void startReturn(@Nullable HitResult result) {
         this.isReturning = true;
         this.setNoPhysics(true);
+        if (this.level().isClientSide())
+            this.getOwner().playSound(SoundEvents.TRIDENT_RETURN);
 
         if (result instanceof BlockHitResult blockHitResult) {
             BiosWrathWeaponsMod.LOGGER.info("startReturn>block: " + blockHitResult.getBlockPos());
@@ -187,11 +217,13 @@ public class ThrownBallsDelightfulPan extends AbstractArrow {
 
     @Nullable
     protected EntityHitResult findHitEntity(Vec3 idk1, Vec3 idk2) {
-        return this.isReturning ? null : super.findHitEntity(idk1, idk2);
+        if (this.isReturning) return null;
+        EntityHitResult result = super.findHitEntity(idk1, idk2);
+        if (result == null || this.hitEntities.contains(result.getEntity())) return null;
+        return result;
     }
 
     protected boolean tryPickup(Player player) {
-//        BiosWrathWeaponsMod.LOGGER.info("tryPickup: " + player);
         return super.tryPickup(player) || this.isNoPhysics() && this.ownedBy(player) && player.getInventory().add(this.getPickupItem());
     }
 
@@ -211,13 +243,15 @@ public class ThrownBallsDelightfulPan extends AbstractArrow {
 
     @Override
     public void tick() {
-//        BiosWrathWeaponsMod.LOGGER.info(isCritArrow());
         Entity owner = this.getOwner();
         if (this.isReturning && owner != null) {
             Vec3 vec3 = this.getOwner().getEyePosition().subtract(this.position());
             this.setDeltaMovement(this.getDeltaMovement().scale(0.95D).add(vec3.normalize().scale(0.18)));
         }
+        if (!this.isReturning && this.lastHitTime != -1 && this.level().getGameTime() - this.lastHitTime > 50)
+            this.startReturn(null);
         super.tick();
+        this.inGround = false; //stoppit abstractarrow >:( the pan is NOT stuck in the ground
     }
 
     @Override
