729d3476d9e21ca1e55f6c69f9a3ec9a1ab1d741
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-09-11 12:38:36 -0500

    phantom essence atytrivutes

--- src/main/java/net/mcreator/bioswrathweapons/item/PhantomEssenceItem.java
+++ src/main/java/net/mcreator/bioswrathweapons/item/PhantomEssenceItem.java
@@ -1,12 +1,22 @@
 package net.mcreator.bioswrathweapons.item;
 
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 import net.mcreator.bioswrathweapons.BiosWrathWeaponsMod;
 import net.mcreator.bioswrathweapons.capability.EssenceDataCapability;
 import net.minecraft.client.gui.screens.EditServerScreen;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import org.checkerframework.checker.units.qual.A;
+import top.theillusivec4.curios.api.SlotContext;
+
+import java.util.UUID;
 
 public class PhantomEssenceItem extends AbstractAbilityEssenceItem {
 
@@ -28,6 +38,14 @@ public class PhantomEssenceItem extends AbstractAbilityEssenceItem {
             cap.updateJumpedFrom(player);
             cap.setHasJumpedFrom(true);
         });
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> attributes = LinkedHashMultimap.create();
+        attributes.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, BiosWrathWeaponsMod.MODID + ":speed_bonus", 0.3, AttributeModifier.Operation.MULTIPLY_BASE));
+        attributes.put(Attributes.MAX_HEALTH, new AttributeModifier(uuid, BiosWrathWeaponsMod.MODID + ":health_bonus", 2, AttributeModifier.Operation.ADDITION));
+        return attributes;
     }
 
     @Override
