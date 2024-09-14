f14e45ebef868263def7f828751dae2b4cd11fab
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2024-09-13 20:03:41 -0500

    robotic essence ability

--- src/main/java/net/mcreator/bioswrathweapons/item/RoboticEssenceItem.java
+++ src/main/java/net/mcreator/bioswrathweapons/item/RoboticEssenceItem.java
@@ -1,20 +1,30 @@
 package net.mcreator.bioswrathweapons.item;
 
+import com.github.sculkhorde.common.entity.SculkMiteAggressorEntity;
+import com.github.sculkhorde.common.entity.SculkMiteEntity;
+import com.github.sculkhorde.common.entity.infection.CursorSurfacePurifierEntity;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import net.mcreator.bioswrathweapons.BiosWrathWeaponsMod;
+import net.mcreator.bioswrathweapons.init.BiosWrathWeaponsModItems;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.phys.AABB;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-public class RoboticEssenceItem extends Item implements ICurioItem {
+public class RoboticEssenceItem extends AbstractAbilityEssenceItem implements ICurioItem {
 
     public RoboticEssenceItem() {
         super(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).fireResistant());
@@ -26,5 +36,27 @@ public class RoboticEssenceItem extends Item implements ICurioItem {
         attributes.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, BiosWrathWeaponsMod.MODID + ":speed_bonus", 0.1, AttributeModifier.Operation.MULTIPLY_BASE));
         attributes.put(Attributes.ARMOR, new AttributeModifier(uuid, BiosWrathWeaponsMod.MODID + ":armor_bonus", 5, AttributeModifier.Operation.ADDITION));
         return attributes;
+    }
+
+    @Override
+    public void useAbility(Player player) {
+        if (!player.level().isClientSide()) {
+            BiosWrathWeaponsMod.LOGGER.info("serverside cursor");
+            int cursorCount = player.getRandom().nextInt(4);
+            for (int i = 0; i < cursorCount; i++) {
+                CursorSurfacePurifierEntity cursor = new CursorSurfacePurifierEntity(player.level());
+                cursor.setPos(BlockPos.containing(player.position().add(0, -1, 0)).getCenter());
+                cursor.setMaxTransformations(200); //same stats as purification flask
+                cursor.setMaxRange(100);
+                cursor.setSearchIterationsPerTick(5);
+                cursor.setMaxLifeTimeMillis(TimeUnit.MINUTES.toMillis(1));
+                cursor.setTickIntervalMilliseconds(150);
+                player.level().addFreshEntity(cursor);
+            }
+        }
+        player.getCooldowns().addCooldown(BiosWrathWeaponsModItems.ROBOTIC_ESSENCE.get(), 18);
+        AABB aabb = new AABB(player.position(), player.position()).inflate(9);
+        player.level().getEntitiesOfClass(SculkMiteEntity.class, aabb).forEach(mite -> mite.hurt(player.damageSources().sonicBoom(player), mite.getHealth()));
+        player.level().getEntitiesOfClass(SculkMiteAggressorEntity.class, aabb).forEach(mite -> mite.hurt(player.damageSources().sonicBoom(player), mite.getHealth()));
     }
 }
